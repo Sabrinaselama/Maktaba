61d1892871fa7d13515d212572690a60de5689bc
SABRINA55X <sabrinaselamaselama@gmail.com>
2026-05-06 08:40:59 +0200

    modif  TP 3

--- app/src/main/java/com/ElOuedUniv/maktaba/presentation/book/BookListView.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/presentation/book/BookListView.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ElOuedUniv.maktaba.data.model.Book
-
+//tp3
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BookListView(
